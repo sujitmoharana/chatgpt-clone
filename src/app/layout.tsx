31cd14aa416ca55dc86253574a03b62818014ab9
Linku Maharana <linkumaharana2@gmail.com>
2026-07-19 08:40:49 +0530

    add react query means tankstack query

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { ThemeProvider } from "@/components/proiders/theme-provider";
+import { QueryProvider } from "@/components/proiders/query-provider";
 
 const inter = Inter({subsets:['latin'],variable:'--font-sans'});
 
@@ -27,10 +29,22 @@ export default function RootLayout({
 }>) {
   return (
     <html
+    suppressHydrationWarning
       lang="en"
       className={cn("h-full", "antialiased", geistSans.variable, geistMono.variable, "font-sans", inter.variable)}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <QueryProvider>
+      <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+        {children}
+        </ThemeProvider>
+        </QueryProvider>
+        </body>
     </html>
   );
 }
---
+++ src/components/proiders/query-provider.tsx
@@ -0,0 +1,18 @@
+import {QueryClient,QueryClientProvider} from "@tanstack/react-query"
+import {ReactQueryDevtools} from "@tanstack/react-query-devtools"
+import * as React from "react"
+
+export function QueryProvider({children}:{children:React.ReactNode}){
+       const [queryClient] = React.useState(()=>new QueryClient({
+        defaultOptions:{
+        queries:{
+            staleTime : 30 * 1000
+        }
+       }}))
+
+       return (
+        <QueryClientProvider client={queryClient}>
+            {children}
+        </QueryClientProvider>
+       )
+}
